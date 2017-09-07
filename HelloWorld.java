76d7ba8437725b6aa76fad00a9187c0f955f110c
tianxiefanfan <31722041+tianxiefanfan@users.noreply.github.com>
2017-09-07 14:49:26 +0800

    Add files via upload
    计算器程序

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+﻿public class HelloWorld {
+	private static int result; // 静态变量，用于存储运行结果
+    
+    public void add(int n) {
+        result = result + n;
+    }
+    public void substract(int n) {
+        result = result - 1;  //Bug: 正确的应该是 result =result-n
+    }
+    public void multiply(int n) {
+    }         // 此方法尚未写好
+    public void divide(int n) {
+        result = result / n;
+    }
+    public void mDivide(int n) {
+    	if (n == 0) throw new IllegalArgumentException("Dividor cannot be 0");
+        result = result / n;
+    }
+    public void square(int n) {
+        result = n * n;
+    }
+    public void squareRoot(int n) {
+        for (; ;) ;            //Bug : 死循环
+    }
+    public void clear() {     // 将结果清零
+        result = 0;
+    }
+    public int getResult() {
+        return result;
+    }
+}
